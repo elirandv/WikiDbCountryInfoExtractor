92fdd2c1248e9f79e0ad8bbacaff3df4b3450646
elirane <eliraneitan@mail.tau.ac.il>
2020-07-09 16:03:34 +0300

    Update geo_qa.py

--- geo_qa.py
+++ geo_qa.py
@@ -10,8 +10,14 @@ if __name__ == '__main__':
         # make_ontology("https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)")
         print("Ontology.nt was added")
     elif command=="question":
+
         # query = parser(sys.argv[2])
+        
+        query = "select ?c where { " \
+            " ?c <"+prefix+"catpital> <"+prefix+"Central_African_Republic>} "
+            
         # get_query(query, "ontology.nt")
+        print("w")
     else:
         # (command!="create" or command!="question"):
         print("Illegal command, please try 'create' or 'question'.")

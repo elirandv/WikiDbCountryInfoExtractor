774343a25d8d94f728e330044baa4a719de05d85
elirane <eliraneitan@mail.tau.ac.il>
2020-07-10 13:29:36 +0300

    1
    1

--- geo_qa.py
+++ geo_qa.py
@@ -1,13 +1,13 @@
 import sys
 from ontology_maker import make_ontology
-from parser import parse
-from sparQL import get_query
+#from parser import parse
+#from sparQL import get_query
 
 if __name__ == '__main__':
     command=sys.argv[1]
     if (command=="create"):  
         print("Createing ontology...")
-        # make_ontology("https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)")
+        make_ontology("https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)")
         print("Ontology.nt was added")
     elif command=="question":
 

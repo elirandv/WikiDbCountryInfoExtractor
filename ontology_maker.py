84fbfd86275f38ae223d38ab5c2819ce0f9cf6a0
ozgranit <ozgranit@gmail.com>
2020-07-10 16:14:12 +0300

    Update ontology_maker.py

--- ontology_maker.py
+++ ontology_maker.py
@@ -195,11 +195,11 @@ def get_country_info(country, url):
     # prime minister
     #get_pm(infoboxlist[0], country)
     # government
-    #get_government(infoboxlist[0], country)
+    get_government(infoboxlist[0], country)
     # area
-    get_area(infoboxlist[0], country)
+    #get_area(infoboxlist[0], country)
     # population
-    get_pop(infoboxlist[0], country)
+    #get_pop(infoboxlist[0], country)
     return 1
 
 

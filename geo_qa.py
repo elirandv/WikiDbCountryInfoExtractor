c4f55e2b7dc18d6458ecb8cab232e28dae33b082
ozgranit <ozgranit@gmail.com>
2020-07-14 13:26:19 +0300

    Update geo_qa.py

--- geo_qa.py
+++ geo_qa.py
@@ -1,11 +1,15 @@
 import sys
 from ontology_maker import call_maker
 from parser import parse
-# from sparQL import get_query
 
 
 def invalid_command():
     print("Illegal command, please try 'create ontology.nt' or 'question <string>'.")
+    exit(0)
+
+
+def invalid_question():
+    print("Invalid question.")
     exit(0)
 
 
@@ -29,13 +33,16 @@ if __name__ == '__main__':
         for i in range(2, len(sys.argv)):
             sentence.append(sys.argv[i])
 
-        query = parse(sentence)
+        answer = parse(sentence)
+        if answer == 'Error':
+            invalid_question()
 
+        print(answer)
         query = "select ?c where { " \
                 " ?c <" + prefix + "capital> <" + prefix + "Central_African_Republic>} "
 
         # get_query(query, "ontology.nt")
-        print("???")
+
         print("WikiDb Country Info Extractor command done.")
     else:
         invalid_command()

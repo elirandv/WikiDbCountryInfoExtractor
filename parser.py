d7cbad051f9f03f39ca0f66579a691d8077d7958
ozgranit <ozgranit@gmail.com>
2020-07-14 13:26:24 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,2 +1,54 @@
+import query
+
+
+def who_ques(question):
+	if len(question) < 3:
+		return 'Error: who question too short'
+	if question[1] != "is":
+		return 'Error'
+	if question[2] == "the":
+		# who is president of..
+		if len(question) < 6:
+			return 'Error: who question too short'
+		if question[3] == "president" and question[4] == "of":
+			return query.who_is_pres(question[5])
+
+		# who is prime minister of..
+		if len(question) < 7:
+			return 'Error: who question too short'
+		if question[3] == "prime" and question[4] == "minister" and question[5] == "of":
+			return query.who_is_pm(question[6])
+
+	# who is <person>
+	if len(question) > 3:
+		return 'Error: who question too long'
+	return query.who_is(question[2])
+
+
+def what_ques(question):
+	return 'Error'
+
+
+def when_ques(question):
+	return 'Error'
+
+
 def parse(question):
-  return question
+	# question is an array of words
+	if len(question) < 1:
+		return 'Error: empty question'
+	if question[len(question)-1].endswith("?"):
+		return 'Error: please end with "?"'
+	# slice '?' from end
+	question[len(question) - 1] = question[len(question)-1][:-1]
+
+	if question[0] == 'Who' or question[0] == 'who':
+		answer = who_ques(question)
+	elif question[0] == 'What' or question[0] == 'what':
+		answer = what_ques(question)
+	elif question[0] == 'When' or question[0] == 'when':
+		answer = when_ques(question)
+	else:
+		answer = 'Error'
+
+	return answer

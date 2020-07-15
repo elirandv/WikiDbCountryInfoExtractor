4c21cd4a109b8ccf278dd9c26939b981fbfbbd98
elirane <eliraneitan@mail.tau.ac.il>
2020-07-15 13:07:13 +0300

    3
    3

--- query.py
+++ query.py
@@ -5,66 +5,48 @@ wiki_prefix = "http://en.wikipedia.org"
 def who_is_pres(country):
     q = "select ?p where { " \
         " ?p <"+president_edge+"> <"+wiki_prefix+country+">} "
-    x1 = ontology.query(q)
-    for line in list(x1):
-        print(line)
-
+    return ontology.query(q)
+    
 def who_is_pm(country):
-	x1 = "select ?p where { " \
+	q = "select ?p where { " \
         " ?p <"+prime_minister_edge+"> <"+wiki_prefix+country+">} "
-    x1 = ontology.query(q)
-    for line in list(x1):
-        print(line)
+    return ontology.query(q)
 
 def what_is_pop(country):
-	x1 = "select ?p where { " \
+	q = "select ?p where { " \
         " ?p <"+population_edge+"> <"+wiki_prefix+country+">} "
-    x1 = ontology.query(q)
-    for line in list(x1):
-        print(line)
+    return ontology.query(q)
         
 def what_is_area(country):
-    x1 = "select ?a where { " \
+    q = "select ?a where { " \
         " ?a <"+area_edge+"> <"+wiki_prefix+country+">} "
-    x1 = ontology.query(q)
-    for line in list(x1):
-        print(line)
+    return ontology.query(q)
         
 def what_is_gov(country):
-	x1 = "select ?g where { " \
+	q = "select ?g where { " \
         " ?g <"+government_edge+"> <"+wiki_prefix+country+">} "
-    x1 = ontology.query(q)
-    for line in list(x1):
-        print(line)
+    return ontology.query(q)
         
 def what_is_cap(country):
-	x1 = "select ?c where { " \
+	q = "select ?c where { " \
         " ?c <"+capitol_edge+"> <"+wiki_prefix+country+">} "
-    x1 = ontology.query(q)
-    for line in list(x1):
-        print(line)
+    return ontology.query(q)
         
 def when_was_pres_born(country):
-	x1 = "select ?d where { " \
+	q = "select ?d where { " \
         " ?p <"+president_edge+"> <"+wiki_prefix+country+">} " \
         " ?p <"+birthDate_edge+"> ?d} "
-    x1 = ontology.query(q)
-    for line in list(x1):
-        print(line)
+    return ontology.query(q)
                
 def when_was_pm_born(country):
-	x1 = "select ?d where { " \
+	q = "select ?d where { " \
         " ?p <"+prime_minister_edge+"> <"+wiki_prefix+country+">} " \
         " ?p <"+birthDate_edge+"> ?d} "
-    x1 = ontology.query(q)
-    for line in list(x1):
-        print(line)
+    return ontology.query(q)
         
 def who_is(person):
-	x1 = "select ?e ?c where { " \
+	q = "select ?e ?c where { " \
         " "+person+" ?e ?c} "
-    x1 = ontology.query(q)
-    for line in list(x1):
-        print(line)
+    return ontology.query(q)
         
 

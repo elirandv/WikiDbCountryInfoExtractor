6ce283e0d15cd02822b3c87b79d443e06857dddd
ozgranit <ozgranit@gmail.com>
2020-07-15 15:14:14 +0300

    Update query.py

--- query.py
+++ query.py
@@ -38,49 +38,48 @@ def who_is_pres(country):
     return answer
 
 
-
 def who_is_pm(country):
     q = "select ?p where { " \
-        " ?p <" + prime_minister_edge + "> <" + wiki_prefix + country + ">} "
+        " <" + wiki_prefix + country + "> <" + prime_minister_edge + "> ?p} "
     return ontology.query(q)
 
 
 def what_is_pop(country):
     q = "select ?p where { " \
-        " ?p <" + population_edge + "> <" + wiki_prefix + country + ">} "
+        " <" + wiki_prefix + country + "> <" + population_edge + "> ?p} "
     return ontology.query(q)
 
 
 def what_is_area(country):
     q = "select ?a where { " \
-        " ?a <" + area_edge + "> <" + wiki_prefix + country + ">} "
+        "  <" + wiki_prefix + country + "> <" + area_edge + "> ?a} "
     return ontology.query(q)
 
 
 def what_is_gov(country):
     q = "select ?g where { " \
-        " ?g <" + government_edge + "> <" + wiki_prefix + country + ">} "
+        " <" + wiki_prefix + country + "> <" + government_edge + "> ?g} "
     return ontology.query(q)
 
 
 def what_is_cap(country):
     q = "select ?c where { " \
-        " ?c <" + capitol_edge + "> <" + wiki_prefix + country + ">} "
+        " <" + wiki_prefix + country + "> <" + capitol_edge + "> ?c} "
     return ontology.query(q)
 
 
 def when_was_pres_born(country):
     q = "select ?d where { " \
-        " ?p <" + president_edge + "> <" + wiki_prefix + country + ">} " \
-                                                                   " ?p <" + birthDate_edge + "> ?d} "
+        " <" + wiki_prefix + country + "> <" + president_edge + "> ?p} " \
+        " ?p <" + birthDate_edge + "> ?d} "
     return ontology.query(q)
 
 
 def when_was_pm_born(country):
     q = "select ?d where { " \
-        " ?p <" + prime_minister_edge + "> <" + wiki_prefix + country + ">} " \
-                                                                        " ?p <" + birthDate_edge + "> ?d} "
-    return ontology.query(q)
+        " <" + wiki_prefix + country + "> <" + prime_minister_edge + "> ?p} " \
+        " ?p <" + birthDate_edge + "> ?d} "
+    return list(ontology.query(q))
 
 
 def who_is(person):

77e405414309d2ed39956afd2b5a3b0c7dcbed52
ozgranit <ozgranit@gmail.com>
2020-07-15 11:08:25 +0300

    Update geo_qa.py

--- geo_qa.py
+++ geo_qa.py
@@ -1,49 +1,32 @@
 import sys
-from ontology_maker import make_ontology
+from ontology_maker import call_maker
 from parser import parse
 from sparQL import get_query
 
+
+def illegal_command():
+	print("Illegal command, please try 'create' or 'question'.")
+	exit(0)
+
+
 if __name__ == '__main__':
-    wiki_prefix = "http://en.wikipedia.org"
-    if len(sys.argv)==1:
-        print("Illegal command, please try 'create' or 'question'.")
-        exit(0)
-    command=sys.argv[1]
-    if (command=="create"):  
-        print("WikiDb Country Info Extractor Createing ontology...")
-        make_ontology("https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)")
-        print("Ontology.nt was added")
-        print("WikiDb Country Info Extractor command done.")
-    elif command=="question":
 
-        # query = parser(sys.argv[2])
-        
-        query = "select ?c where { " \
-            " ?c <"+wiki_prefix+"catpital> <"+wiki_prefix+"Central_African_Republic>} "
-            
-        get_query(query, "ontology.nt")
-        print("???")
-        print("WikiDb Country Info Extractor command done.")
-    else:
-        # (command!="create" or command!="question"):
-        print("Illegal command, please try 'create' or 'question'.")
+	if len(sys.argv) == 1:
+		illegal_command()
 
+	command = sys.argv[1]
+	if command == "create":
+		print("WikiDb Country Info Extractor Creating ontology...")
+		call_maker()
+		print("WikiDb Country Info Extractor command done.")
+	elif command == "question":
+		sentence = []
+		for i in range(2, len(sys.argv)):
+			sentence.append(word)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#can u see this change??????
+		answer = parse(sentence)
+		print(answer)
+		print("WikiDb Country Info Extractor command done.")
+	else:
+		# command!="create" or command!="question"
+		illegal_command()
